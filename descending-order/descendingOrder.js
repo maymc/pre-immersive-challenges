89efd2c939f5c1a2ef367b3512077609ea618788
May Chen <maychen@Mays-MacBook-Pro.local>
2018-06-07 21:21:13 -1000

    Completed challenge

--- descending-order/descendingOrder.js
+++ descending-order/descendingOrder.js
@@ -1,3 +1,19 @@
 module.exports = function descendingOrder(number) {
   // write code in here
+  if(typeof number !== "number"){
+    return 'not a number!';
+  }
+
+  numString = number.toString();
+  console.log(numString);
+    
+  numArray = numString.split("");
+  console.log(numArray);
+
+    numArray.sort(function(a, b){return b - a})
+    console.log(numArray);
+
+    var combinedNum = numArray.join("");
+    console.log(combinedNum);
+    return combinedNum;
 }
