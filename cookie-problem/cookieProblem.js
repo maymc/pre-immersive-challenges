a9711c41893359a11f6a9c6f136db31a7c36e71b
May Chen <maychen@Mays-MacBook-Pro.local>
2018-06-07 16:56:26 -1000

    Completed this coding challenge

--- cookie-problem/cookieProblem.js
+++ cookie-problem/cookieProblem.js
@@ -1,3 +1,33 @@
 module.exports = function cookieProblem(array) {
   // write your code in here
+
+  //Initialize variables
+  var highestAmtOfCookies = 0;
+  var cookieDiff = 0;
+  var additionalCookies = 0;
+
+  //Get the highest num of cookies
+  for(var i = 0; i<array.length; i++){
+    //console.log("current highest cookie amount: " + highestAmtOfCookies);
+
+    //If this person has the most cookies, update the highest cookie amount
+    if(array[i] > highestAmtOfCookies){
+      highestAmtOfCookies = array[i];    }
+  }
+
+  //console.log("Final highest cookie amt count: " + highestAmtOfCookies);
+
+  //Loop through the array and determine how many more cookies need to be given per person using the difference from the highest cookie count and then total the amount of cookies added
+  for(var j = 0; j<array.length; j++){
+    if(array[j] !== highestAmtOfCookies){
+      cookieDiff = highestAmtOfCookies - array[j];
+      //console.log("cookie difference :" + cookieDiff);
+      additionalCookies += cookieDiff;
+      console.log("Total Cookies to add: " + additionalCookies);
+    }
+  }
+
+  return additionalCookies;
+
+
 }
