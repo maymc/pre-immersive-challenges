f027773bd28e37d823cc841093269473770bb85f
May Chen <maychen@Mays-MacBook-Pro.local>
2018-06-07 17:03:35 -1000

    Completed this coding challenge

--- count-by-multiples/countByMultiples.js
+++ count-by-multiples/countByMultiples.js
@@ -1,3 +1,14 @@
 module.exports = function countByMultiples(length, x) {
   // write your code in here
+
+  //Initialize an empty array for the elements
+  var elemArray = [];
+
+  for(var i = 1; i<=length; i++){
+    multipleOfX = x*i;
+    elemArray.push(multipleOfX);
+    console.log("Current array: " + elemArray);
+  }
+
+  return elemArray;
 }
