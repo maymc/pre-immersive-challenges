e163dd570ecbd33c5c9107932997e070cbf26420
May Chen <maychen@Mays-MacBook-Pro.local>
2018-06-18 21:54:55 -1000

    completed challenge

--- find-middle-value/findMiddleValue.js
+++ find-middle-value/findMiddleValue.js
@@ -1,3 +1,22 @@
 module.exports = function findMiddleValue(numArray) {
   // write your code in here
+
+  //copy the original array
+  var numArray2 = numArray.slice();
+  //console.log("numArray2: " + numArray2);
+
+  //sort the copied array into numerical order
+  var sortedArray = numArray2.sort(function(a, b){return a-b});
+  //console.log("sortedArray: " + sortedArray);
+
+
+  //the middle number will be at index 1 of the sorted array of 3 numbers
+  var middleNum = sortedArray[1]; 
+  //console.log("middleNum: " + middleNum);
+
+  var middleNumIndex = numArray.indexOf(middleNum);
+  //console.log("middleNumIndex: " + middleNumIndex);
+
+  return middleNumIndex;
+
 }
