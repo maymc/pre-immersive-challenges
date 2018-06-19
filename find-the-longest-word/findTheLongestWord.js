e51a8a82a8b8e01781dd2e6a6ad595cad5686157
May Chen <maychen@Mays-MacBook-Pro.local>
2018-06-18 22:00:01 -1000

    completed challenge

--- find-the-longest-word/findTheLongestWord.js
+++ find-the-longest-word/findTheLongestWord.js
@@ -1,3 +1,14 @@
 module.exports = function findTheLongestWord(sen) {
   // write your code in here
+
+  var wordLength = 0;
+  var longestWord;
+
+  for(var i=0; i<sen.length; i++){
+    if(wordLength < sen[i].length){
+      wordLength = sen[i].length;
+      longestWord = sen[i];
+    }
+  }
+  return longestWord;
 }
