9ed71d28bfcd61165130b10d3f27dd6fa8fce9f4
May Chen <maychen@Mays-MacBook-Pro.local>
2018-06-18 22:21:36 -1000

    completed challenge

--- find-unique-num/findUniqueNum.js
+++ find-unique-num/findUniqueNum.js
@@ -1,3 +1,25 @@
 module.exports = function findUniqueNum(strOfNums) {
   // write your code in here
+
+  var strArray = strOfNums.split(" ");
+  console.log(strArray);
+
+  //check if array is odd
+  if(strArray[0] % 2 !== 0 && strArray[1] !== 0){
+    console.log("array is odd");
+    for(var i=0; i<strArray.length; i++){
+      if(strArray[i] % 2 === 0){
+        return strArray.indexOf(strArray[i]);
+      }
+    }
+  }
+  //else array is odd
+  else{
+    console.log("array is even");
+    for(var i=0; i<strArray.length; i++){
+      if(strArray[i] % 2 !== 0){
+        return strArray.indexOf(strArray[i]);
+      }
+    }
+  }
 }
