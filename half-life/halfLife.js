9fc4a1102af04af52f6bcfcf47d926c021b83410
May Chen <maychen@Mays-MacBook-Pro.local>
2018-06-20 00:31:16 -1000

    Attempted to complete this coding challenge in a different way

--- half-life/halfLife.js
+++ half-life/halfLife.js
@@ -1,3 +1,14 @@
-module.exports = function halfLife(initialQuantity, quantityRemaining, time) {
-  return time / Math.log2(initialQuantity / quantityRemaining);
+// module.exports = function halfLife(initialQuantity, quantityRemaining, time) {
+//   return time / Math.log2(initialQuantity / quantityRemaining);
+// }
+
+module.exports = function halfLife(initialQty, remainingQty, time){
+    if(initialQty/2 === remainingQty){
+        //remaining qty is one half of the atoms; time periods listed is time it took
+        return time;
+    }
+    //If remaining qty is not half, then half life came earlier than the time period
+    else{
+        return time/(Math.log2(initialQty/remainingQty));
+    }
 }
