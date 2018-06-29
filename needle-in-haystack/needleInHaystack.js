20ab2011137f10b4392a1f396aa808c60c28989c
May Chen <maychen@Mays-MacBook-Pro.local>
2018-06-28 21:54:08 -1000

    completed challenge

--- needle-in-haystack/needleInHaystack.js
+++ needle-in-haystack/needleInHaystack.js
@@ -1,3 +1,10 @@
 module.exports = function needleInHaystack(haystack) {
-  // write code in here
+
+  var haystackFlag = haystack.includes("needle");
+  if(haystackFlag === true){
+    return haystack.indexOf("needle");
+  }
+  else{
+    return haystackFlag;
+  }
 }
