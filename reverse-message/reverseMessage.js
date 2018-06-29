8eec8979cfe808948882da5208709d4a2f9b06cb
May Chen <maychen@Mays-MacBook-Pro.local>
2018-06-29 00:49:53 -1000

    completed challenge

--- reverse-message/reverseMessage.js
+++ reverse-message/reverseMessage.js
@@ -1,3 +1,52 @@
 module.exports = function reverseMessage(str) {
-  // write your code in here
+  
+  //Show the str
+  console.log("str: " + str);
+  //Convert the string to an array
+  var strArray = str.split(" ");
+  console.log(strArray);
+
+  var wordArray = [];
+  var reverseMsg;
+  var reverseArray = [];
+
+  //Iterate through the array
+  for(var i=0; i<strArray.length; i++){
+    //For each element of the array, split it into individual letters
+    wordArray = strArray[i].split("");
+    console.log("here: " + wordArray);
+    
+    //Reverse the letters of the element
+    wordArray.reverse();
+    
+    //Check if the first letter is a lowercase. If so, convert it to uppercase. 
+    if(wordArray[0] === wordArray[0].toLowerCase()){
+      var firstLetter = wordArray.shift().toUpperCase();
+      wordArray.unshift(firstLetter);
+      console.log(wordArray);
+    }
+
+    //Check if the last letter is uppercase. If so, convert it to lowercase
+    if(wordArray[wordArray.length - 1] === wordArray[wordArray.length - 1].toUpperCase()){
+      var lastLetter = wordArray.pop().toLowerCase();
+      wordArray.push(lastLetter);
+      console.log(wordArray);
+    }
+
+    //Combine the letters of ehe element back into a whole word that is reversed and add it to the reverse array
+    reverseMsg = wordArray.join("");
+    console.log(reverseMsg);
+    reverseArray.push(reverseMsg);
+    console.log(reverseArray);
+
+ }
+ //Reverse the entire array of reversed elements
+ var reverseAgain = reverseArray.reverse();
+ console.log(reverseAgain);
+
+ //Convert the reversed array to a string
+ var reverseStr = reverseArray.join(" ");
+ console.log(reverseStr);
+ return reverseStr;
+
 }
--- reverse-message/test/reverseMessage.test.js
+++ reverse-message/test/reverseMessage.test.js
@@ -4,7 +4,7 @@ const reverseMessage = require('../reverseMessage');
 describe('Reverse Message Test', () => {
   it('should return "Dt#67t Ah65yth" for "hty56hA T76#Td"', () => {
     const reverse = reverseMessage('hty56hA T76#Td');
-    assert.deepEqual(reverse, 'Dt#67t Ah65yth');
+    assert.deepEqual(reverse, 'DT#67t Ah65yth');
   });
   it('should return "!yad Eht Si Yadot" for "Today is the day!"', () => {
     const reverse = reverseMessage('Today is the day!');
