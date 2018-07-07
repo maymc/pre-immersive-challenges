a267cd8d932a61a64e6b468c4ec25c1ec32686a5
May Chen <maychen@Mays-MacBook-Pro.local>
2018-07-06 23:57:56 -1000

    completed challenge

--- swap-letter-case/swapLetterCase.js
+++ swap-letter-case/swapLetterCase.js
@@ -1,3 +1,23 @@
 module.exports = function swapLetterCase(str) {
-  // write your code in here
+  console.log("string: " + str);
+
+  var arr2 = [];
+
+  for(var i=0; i<str.length; i++){
+    if(str[i] === str[i].toUpperCase()){
+      var changeLetter = str[i].toLowerCase();
+      arr2.push(changeLetter);
+      console.log("arr2: " + arr2);
+    }
+    else if(str[i] === str[i].toLowerCase()){
+      var changeLetter = str[i].toUpperCase();
+      arr2.push(changeLetter);
+      console.log("arr2: " + arr2);
+    }
+  }
+
+  var str2 = arr2.join("");
+  console.log("new string: " + str2);
+
+  return str2;
 }
